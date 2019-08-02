97e7341063d55e74c0c198ec36710a95ffa9d57b
Jay M. Wong <jay@southieautonomyworks.com>
2019-08-02 13:56:44 -0400

    Pushing changes to aruco

---
+++ aruco_ros/src/aruco_generic.cpp
@@ -0,0 +1,215 @@
+/*****************************
+Copyright 2011 Rafael Muñoz Salinas. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY Rafael Muñoz Salinas ''AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Rafael Muñoz Salinas OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those of the
+authors and should not be interpreted as representing official policies, either expressed
+or implied, of Rafael Muñoz Salinas.
+********************************/
+/**
+* @file simple_single.cpp
+* @author Bence Magyar
+* @date June 2012
+* @version 0.1
+* @brief ROS version of the example named "simple" in the Aruco software package.
+*/
+
+#include <iostream>
+#include <aruco/aruco.h>
+#include <aruco/cvdrawingutils.h>
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <aruco_ros/aruco_ros_utils.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <visualization_msgs/Marker.h>
+
+#include <dynamic_reconfigure/server.h>
+#include <aruco_ros/ArucoThresholdConfig.h>
+using namespace aruco;
+
+class ArucoGeneric
+{
+private:
+  cv::Mat inImage;
+  aruco::CameraParameters camParam;
+  tf::StampedTransform rightToLeft;
+  bool useRectifiedImages;
+  MarkerDetector mDetector;
+  vector<Marker> markers;
+  ros::Subscriber cam_info_sub;
+  bool cam_info_received;
+  image_transport::Publisher image_pub;
+  image_transport::Publisher debug_pub;
+  ros::Publisher pose_pub;
+  ros::Publisher transform_pub;
+  ros::Publisher position_pub;
+  ros::Publisher marker_pub; //rviz visualization marker
+  ros::Publisher pixel_pub;
+  std::string marker_frame;
+  std::string camera_frame;
+  std::string reference_frame;
+
+  double marker_size;
+  int marker_id;
+  bool rotate_marker_axis_;
+
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it;
+  image_transport::Subscriber image_sub;
+
+  tf::TransformListener _tfListener;
+
+  dynamic_reconfigure::Server<aruco_ros::ArucoThresholdConfig> dyn_rec_server;
+
+public:
+  ArucoGeneric()
+    : cam_info_received(false),
+      nh("~"),
+      it(nh)
+  {
+
+    std::string refinementMethod;
+    nh.param<std::string>("corner_refinement", refinementMethod, "LINES");
+    if ( refinementMethod == "SUBPIX" )
+      mDetector.setCornerRefinementMethod(aruco::MarkerDetector::SUBPIX);
+    else if ( refinementMethod == "HARRIS" )
+      mDetector.setCornerRefinementMethod(aruco::MarkerDetector::HARRIS);
+    else if ( refinementMethod == "NONE" )
+      mDetector.setCornerRefinementMethod(aruco::MarkerDetector::NONE);
+    else
+      mDetector.setCornerRefinementMethod(aruco::MarkerDetector::LINES);
+
+    //Print parameters of aruco marker detector:
+    ROS_INFO_STREAM("Corner refinement method: " << mDetector.getCornerRefinementMethod());
+    ROS_INFO_STREAM("Threshold method: " << mDetector.getThresholdMethod());
+    double th1, th2;
+    mDetector.getThresholdParams(th1, th2);
+    ROS_INFO_STREAM("Threshold method: " << " th1: " << th1 << " th2: " << th2);
+    float mins, maxs;
+    mDetector.setMinMaxSize(0.025, 0.5);
+    mDetector.getMinMaxSize(mins, maxs);
+    ROS_INFO_STREAM("Marker size min: " << mins << "  max: " << maxs);
+    ROS_INFO_STREAM("Desired speed: " << mDetector.getDesiredSpeed());
+
+
+
+    image_sub = it.subscribe("/image", 1, &ArucoGeneric::image_callback, this);
+    cam_info_sub = nh.subscribe("/camera_info", 1, &ArucoGeneric::cam_info_callback, this);
+
+    image_pub = it.advertise("result", 1);
+    debug_pub = it.advertise("debug", 1);
+    pose_pub = nh.advertise<geometry_msgs::PoseStamped>("pose", 100);
+    transform_pub = nh.advertise<geometry_msgs::TransformStamped>("transform", 100);
+    position_pub = nh.advertise<geometry_msgs::Vector3Stamped>("position", 100);
+    marker_pub = nh.advertise<visualization_msgs::Marker>("marker", 10);
+    pixel_pub = nh.advertise<geometry_msgs::PointStamped>("pixel", 10);
+
+    nh.param<double>("marker_size", marker_size, 0.05);
+    nh.param<int>("marker_id", marker_id, 300);
+    nh.param<std::string>("reference_frame", reference_frame, "");
+    nh.param<std::string>("camera_frame", camera_frame, "");
+    nh.param<std::string>("marker_frame", marker_frame, "");
+    nh.param<bool>("image_is_rectified", useRectifiedImages, true);
+    nh.param<bool>("rotate_marker_axis", rotate_marker_axis_, true);
+
+    ROS_ASSERT(camera_frame != "" && marker_frame != "");
+
+    if ( reference_frame.empty() )
+      reference_frame = camera_frame;
+
+    ROS_INFO("Aruco node started with marker size of %f m and marker id to track: %d",
+             marker_size, marker_id);
+    ROS_INFO("Aruco node will publish pose to TF with %s as parent and %s as child.",
+             reference_frame.c_str(), marker_frame.c_str());
+
+    dyn_rec_server.setCallback(boost::bind(&ArucoGeneric::reconf_callback, this, _1, _2));
+  }
+
+
+  void image_callback(const sensor_msgs::ImageConstPtr& msg){
+
+    static tf::TransformBroadcaster br;
+
+    ros::Time curr_stamp(ros::Time::now());
+    cv_bridge::CvImagePtr cv_ptr;
+    markers.clear();
+
+    try {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::RGB8);
+      inImage = cv_ptr->image;
+
+      mDetector.detect(inImage, markers, camParam, -1, false);
+      std::cout << "** Aruco detections => " << markers.size() << "\n";
+
+      for (size_t i = 0; i < markers.size(); i++){
+        std::cout << "  > " << markers[i].id << " => " << markers[i].getCenter().x << " " << markers[i].getCenter().y << "\n";
+      }
+    }
+    catch (cv_bridge::Exception& e) {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+  }
+
+  // wait for one camerainfo, then shut down that subscriber
+  void cam_info_callback(const sensor_msgs::CameraInfo &msg)
+  {
+    camParam = aruco_ros::rosCameraInfo2ArucoCamParams(msg, useRectifiedImages);
+
+    // handle cartesian offset between stereo pairs
+    // see the sensor_msgs/CamaraInfo documentation for details
+    rightToLeft.setIdentity();
+    rightToLeft.setOrigin(
+        tf::Vector3(
+            -msg.P[3]/msg.P[0],
+            -msg.P[7]/msg.P[5],
+            0.0));
+
+    cam_info_received = true;
+    cam_info_sub.shutdown();
+  }
+
+
+  void reconf_callback(aruco_ros::ArucoThresholdConfig &config, uint32_t level)
+  {
+    mDetector.setThresholdParams(config.param1,config.param2);
+    if (config.normalizeImage)
+    {
+      ROS_WARN("normalizeImageIllumination is unimplemented!");
+    }
+  }
+};
+
+
+int main(int argc,char **argv)
+{
+  ros::init(argc, argv, "aruco_simple");
+
+  ArucoGeneric node;
+
+  ros::spin();
+}
